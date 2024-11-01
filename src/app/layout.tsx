4b3948b65a7156bee1b6200f314b34dcfc8fd789
xinghe98 <lcg9899@gmail.com>
2024-11-01 21:29:58 +0800

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "李星河的个人博客",
+  description: "个人项目",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="zh_CN">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import ShimmerButton from "@/components/ui/shimmer-button";
+
+export default function Home() {
+  return (
+    <div>
+      test page
+      <ShimmerButton>Shimmer Button</ShimmerButton>
+    </div>
+  );
+}
