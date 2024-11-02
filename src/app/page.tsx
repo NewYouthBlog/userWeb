8b819d78d6462c2d8b1cad8b5265143c7563f862
xinghe98 <lcg9899@gmail.com>
2024-11-02 09:42:05 +0800

    tailwind css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
-import ShimmerButton from "@/components/ui/shimmer-button";
+import Footer from "@/components/footer";
+import { Container } from "@mui/material";
 
 export default function Home() {
   return (
-    <div>
-      test page
-      <ShimmerButton>Shimmer Button</ShimmerButton>
-    </div>
+    <>
+      <Container
+        maxWidth="lg"
+        component="main"
+        sx={{ display: "flex", flexDirection: "column", my: 16, gap: 4 }}
+      >
+        {/* <MainContent /> */}
+      </Container>
+      <Footer />
+    </>
   );
 }
