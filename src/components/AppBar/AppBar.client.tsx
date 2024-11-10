72071434accb9973e73b3ae96876c297b59adc8b
xinghe98 <lcg9899@gmail.com>
2024-11-10 11:26:09 +0800

    update

--- src/components/AppBar/AppBar.client.tsx
+++ src/components/AppBar/AppBar.client.tsx
@@ -4,86 +4,83 @@ import HomeIcon from "@mui/icons-material/Home";
 import { Button, createTheme, styled, ThemeProvider } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import IconButton from "@mui/material/IconButton";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import { usePathname, useRouter } from "next/navigation";
 
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#d9e2e7",
-    },
-    secondary: {
-      main: "#154b77",
-      contrastText: "#afc9db",
-    },
-  },
+	palette: {
+		primary: {
+			main: "#d9e2e7",
+		},
+		secondary: {
+			main: "#154b77",
+			contrastText: "#afc9db",
+		},
+	},
 });
 const StyledAppBar = styled(AppBar)(({ theme }) => ({
-  backdropFilter: "blur(10px)", // 设置模糊效果
-  backgroundColor: "rgba(255, 255, 255, 0.3)", // 设置半透明背景
-  boxShadow: "none",
+	backdropFilter: "blur(10px)", // 设置模糊效果
+	backgroundColor: "rgba(255, 255, 255, 0.3)", // 设置半透明背景
+	boxShadow: "none",
 }));
 
 export default function AppBarClient() {
-  const router = useRouter();
-  const pathname = usePathname();
+	const router = useRouter();
+	const pathname = usePathname();
 
-  //FIX:使用后端,最好是写道上一层组件，保证ssr
-  const pathMap = [
-    { labelText: "主页", icon: <HomeIcon />, routerPath: "/" },
-    { labelText: "归档", icon: <ArchiveIcon />, routerPath: "/archive" }, // 假设还有其他图标
-    // 可以在这里添加更多的项目
-  ];
+	//FIX:使用后端,最好是写道上一层组件，保证ssr
+	const pathMap = [
+		{ labelText: "主页", icon: <HomeIcon />, routerPath: "/" },
+		{ labelText: "归档", icon: <ArchiveIcon />, routerPath: "/archive" }, // 假设还有其他图标
+		// 可以在这里添加更多的项目
+	];
 
-  return (
-    <ThemeProvider theme={theme}>
-      <StyledAppBar position="fixed" color="transparent">
-        <Toolbar>
-          <Box sx={{ flexGrow: 1 }} />
-          {pathMap.map((item, index) => (
-            <Button
-              key={index} // 使用唯一的 key
-              size="small"
-              aria-label={item.labelText}
-              sx={{
-                mr: 2,
-                color:
-                  pathname === item.routerPath
-                    ? theme.palette.secondary.main
-                    : theme.palette.secondary.contrastText,
-              }}
-              onClick={() => router.push(item.routerPath)}
-            >
-              <Typography
-                component="div"
-                className="font-custom flex items-center"
-                sx={{
-                  position: "relative", // 设置为相对定位，用于控制伪元素的位置
-                  paddingBottom: 1, // 增加一些底部空间
-                  "&::after": {
-                    content: '""',
-                    position: "absolute",
-                    left: 0,
-                    bottom: 0,
-                    width: "100%",
-                    height: "2px",
-                    backgroundColor:
-                      pathname === item.routerPath
-                        ? theme.palette.secondary.main
-                        : "transparent",
-                    transition: "background-color 0.3s ease", // 使用平滑的背景色变化
-                  },
-                }}
-              >
-                {item.icon}
-                {item.labelText}
-              </Typography>
-            </Button>
-          ))}
-        </Toolbar>
-      </StyledAppBar>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<StyledAppBar position="fixed" color="transparent">
+				<Toolbar>
+					<Box sx={{ flexGrow: 1 }} />
+					{pathMap.map((item, index) => (
+						<Button
+							key={index} // 使用唯一的 key
+							size="small"
+							aria-label={item.labelText}
+							sx={{
+								mr: 2,
+								color:
+									pathname === item.routerPath
+										? theme.palette.secondary.main
+										: theme.palette.secondary.contrastText,
+							}}
+							onClick={() => router.push(item.routerPath)}
+						>
+							<Typography
+								component="div"
+								className="font-custom flex items-center"
+								sx={{
+									position: "relative", // 设置为相对定位，用于控制伪元素的位置
+									paddingBottom: 1, // 增加一些底部空间
+									"&::after": {
+										content: '""',
+										position: "absolute",
+										left: 0,
+										bottom: 0,
+										width: "100%",
+										height: "2px",
+										backgroundColor:
+											pathname === item.routerPath ? theme.palette.secondary.main : "transparent",
+										transition: "background-color 0.3s ease", // 使用平滑的背景色变化
+									},
+								}}
+							>
+								{item.icon}
+								{item.labelText}
+							</Typography>
+						</Button>
+					))}
+				</Toolbar>
+			</StyledAppBar>
+		</ThemeProvider>
+	);
 }
