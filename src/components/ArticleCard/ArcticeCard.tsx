29a50b778825884fb5a03f858ada759d30566499
xinghe98 <lcg9899@gmail.com>
2024-11-14 22:44:16 +0800

    分页器

--- src/components/ArticleCard/ArcticeCard.tsx
+++ src/components/ArticleCard/ArcticeCard.tsx
@@ -1,7 +1,7 @@
 import { article } from "@/@types/arctice";
 import CardfoPC from "./CardforPC";
 import CardforMobile from "./CardforMobile";
-import { Box } from "@mui/material";
+import { Box, Pagination } from "@mui/material";
 import Link from "next/link";
 import request from "@/lib/request";
 import { AxiosResponse } from "axios";
@@ -14,15 +14,40 @@ export default async function ArcticCard() {
 
   const listArticle = res.data.data.articles.map((item, index) => {
     return (
-      <Link key={index} href={`/articles/${item.id}`} legacyBehavior>
-        <a target="_blank">
-          <Box sx={{ mt: 4 }} key={index}>
-            <CardfoPC key={`${index}-pc`} data={item}></CardfoPC>
-            <CardforMobile key={`${index}-mob`} data={item}></CardforMobile>
-          </Box>
-        </a>
-      </Link>
+      <>
+        <Link key={index} href={`/articles/${item.id}`} legacyBehavior>
+          <a target="_blank">
+            <Box sx={{ mt: 4 }} key={index}>
+              <CardfoPC key={`${index}-pc`} data={item}></CardfoPC>
+              <CardforMobile key={`${index}-mob`} data={item}></CardforMobile>
+            </Box>
+          </a>
+        </Link>
+      </>
     );
   });
-  return <>{listArticle}</>;
+  return (
+    <>
+      {listArticle}
+      <Box
+        sx={{
+          mt: 5,
+          display: "flex",
+          justifyContent: "flex-end",
+        }}
+      >
+        {/* FIX: */}
+        <Pagination
+          count={res.data.data.total}
+          color="primary"
+          sx={{
+            "& .MuiPaginationItem-root.Mui-selected": {
+              backgroundColor: "#538bb5", // 选中状态颜色
+              color: "white", // 选中文字颜色
+            },
+          }}
+        />
+      </Box>
+    </>
+  );
 }
