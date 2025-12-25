1e80e1a87baab8a829674e1c7d8a06d463a61842
xinghe98 <lcg9899@gmail.com>
2025-12-25 21:01:07 +0800

    a

--- src/lib/request.ts
+++ src/lib/request.ts
@@ -10,7 +10,7 @@ const getBaseUrl = () => {
     // 这里必须使用 Docker 内部的绝对路径！
     // 格式：http://<docker-compose里的服务名>:<端口>
     // 例如：http://backend-service:3000
-    return process.env.INTERNAL_API_URL || 'http://127.0.0.1:3000';
+    return process.env.API_URL || 'http://127.0.0.1:3000';
   }
 
   // 2. 如果是在客户端 (浏览器)
